6244cbca2b13ba492529cfac9c441b1641723dd2
Manuel Pierre <pierrem1.mp@gmail.com>
2020-12-04 22:44:04 -0500

    worked on localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,33 +1,49 @@
-// Set the body to a variable
-// var body = document.body;
-// var currentDate;
-
-// Create all necessary elements
-// var h1El = document.createElement("h1");
-// currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
-
-// // Set the text content of relevant elements
-// h1El.textContent = currentDate;
-
-// body.appendChild(h1El);
-
-// var appointText = "";
-// var appointTime = "";
-// var currentDate;
-// var currentTime;
-// var currentContainer;
-// var tempArray = [];
-// var storedAppointments;
-// var returnedAppointments;
-
-// $(document).on("load", function () {
-// currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
-//   $("#currentDay").append(currentDate);
-// });
-
-// $("#currentDay").ready(function () {
-//   currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
-//   $("#currentDay").append(currentDate);
-// });
-
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+$("hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-1 .description").val(localStorage.getItem("hour-1"));
+$("#hour-2 .description").val(localStorage.getItem("hour-3"));
+$("#hour-3 .description").val(localStorage.getItem("hour-3"));
+$("#hour-4 .description").val(localStorage.getItem("hour-4"));
+$("#hour-5 .description").val(localStorage.getItem("hour-5"));
+
+$(".saveBtn").on("click", function () {
+  event.preventDefault();
+
+  var value = $(".description").val();
+  var appointment = $(this).parent().attr("id");
+
+  localStorage.setItem(appointment, value);
+});
+
+function coordinate() {
+  var currentHour = moment().format("H");
+  var numValue = $("hour-9").val("id");
+  var row = $("hour-9");
+  trueValue = numValue.prevObject[0].nodeType;
+  console.log(numValue);
+  //   console.log(value.prevObject[0].nodeType);
+
+  console.log(currentHour);
+  $(".time-block").each(function () {
+    console.log(trueValue);
+
+    if (trueValue < currentHour) {
+      $(this).addClass("past");
+    } else if (trueValue === currentHour) {
+      $(this).removeClass("past");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("present");
+      $(this).addClass("future");
+    }
+  });
+}
+
+coordinate();
+
+
